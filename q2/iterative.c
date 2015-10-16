9b58f393ad70adc74ea9e69cb0e1e00bd5f01fd6
RC925 <ray.chang925@gmail.com>
2015-10-17 02:16:06 +0800

    To implement Q2(Iterative)
    Take the ref. character to compare characters in a sorted array, return a character strictly larger than the ref. character, if fail, return
    the smallest character in the array.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,29 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+
+char smallest_character(char[], char);
 
 int main()
 {
+
+    char sortedArray[] = {'c','f','j','p','v'};
+    char ref = 'a';
+
+    printf("Output : %c\n", smallest_character(sortedArray, ref));
+
     return 0;
 }
+
+char smallest_character(char str[], char c)
+{
+
+    int i;
+
+    for (i=0 ; i<strlen(str) ; i++) {
+        if (str[i] > c)
+            return str[i];
+    }
+    return str[0];
+}
+
