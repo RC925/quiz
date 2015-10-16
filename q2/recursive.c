650070bf18922d74aca9fe9a417993d4e4e6ac74
RC925 <ray.chang925@gmail.com>
2015-10-17 02:22:26 +0800

    To implement Q2(Recursive)
    Take the ref. character to compare characters in a sorted array, return a character strictly larger than the ref. character, if fail, return
    the smallest character in the array.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,30 @@
 /* FIXME: Implement! */
 
+#include <stdio.h>
+#include <string.h>
+
+char smallest_character(char[], char, int);
+
 int main()
 {
+
+    char sortedArray[] = {'c','f','j','p','v'};
+    char ref = 'a';
+    int index = 0;
+
+    printf("Output : %c\n", smallest_character(sortedArray, ref, index));
+
     return 0;
+}
+
+char smallest_character(char str[], char c, int i)
+{
+
+    if (i == strlen(str))
+        return str[0];
+    else if (str[i] > c)
+        return str[i];
+    else
+        return smallest_character(str, c, ++i);
+
 }
