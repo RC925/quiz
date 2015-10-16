e4f864676026d2f1a55adb3f3f707a81a46eeefb
RC925 <ray.chang925@gmail.com>
2015-10-17 02:31:26 +0800

    To implement Q4(Iterative)
    Use an iterative method to find the contiguous subarray within an array(containing at least one number) which has the largest sum.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,29 @@
-/* FIXME: Implement! */
+#include <stdio.h>
 
-int main()
+int max_subArray_sum(int[], int);
+
+int main ()
 {
-    return 0;
+    int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int num = sizeof(A)/sizeof(int);
+    printf(" The maximum sum of subArray is %d\n",
+           max_subArray_sum(A,num));
+
+}
+
+int max_subArray_sum(int A[], int n)
+{
+    int max_sum = 0x80000000;
+    int sum = 0;
+    int row, col, index;
+
+    for (row=0; row < n ; row++) {
+        for (col=n ; col > row; col--) {
+            for (index=row ; index < col; index++)
+                sum += A[index];
+            if (sum > max_sum) max_sum = sum;
+            sum = 0;
+        }
+    }
+    return max_sum;
 }
